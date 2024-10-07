5dd061b9249f9cb8f5c2805377ba6cd953c42c7a
Shalsa2006 <shalsaberliana00@gmail.com>
2024-10-07 10:06:45 +0700

    Add files via upload

---
+++ menentukan bilangan postif negatif.shalsa berliana F.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input any number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num < 0)
+        {
+            Console.WriteLine("This number negative");
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine("This number positive");
+        }
+        else
+        {
+            Console.WriteLine("This number is zero");
+        }
+        }
+    }
